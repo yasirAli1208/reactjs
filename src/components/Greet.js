1c799ed2df841ced615f68fdd43f46c7cc865d3e
yasirAli1208 <ammarali1208@gmail.com>
2021-03-08 21:53:35 +0500

    first commit

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import  React  from 'react';
+
+// function Greet(){
+//     return <h1>Hello World</h1>
+// }
+
+//with ES6
+// export const Greet = ()=> <h1>Hello World</h1>
+const Greet = ()=> <h1>Hello World</h1>
+
+export default Greet;
